3bd193210c2cf92f778b7f4ea9b1d31ab0dfc74d
Eric Carvajal <carvajal.cortes.e19@gmail.com>
2021-10-24 00:44:55 -0300

    V10 TOKEN DE LOGIN CREADO :)

--- Pokemon/AppUser/login.py
+++ Pokemon/AppUser/login.py
@@ -3,13 +3,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 
-from .serializer import UserTokenSerializer
+from AppUser.serializer import UserTokenSerializer
 
 class Login(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
         login_serializer = self.serializer_class(data = request.data, context = {'request': request})
         if login_serializer.is_valid():
+            print(login_serializer.validated_data['user'])
             user = login_serializer.validated_data['user']
             if user.is_active:
                 token,created = Token.objects.get_or_create(user = user)
@@ -20,8 +21,16 @@ class Login(ObtainAuthToken):
                         'user': user_serializer.data,
                         'message': 'Login Success!'
                     }, status = status.HTTP_201_CREATED)
+                else:
+                    token.delete()
+                    token = Token.objects.create(user = user)
+                    return Response({
+                        'token': token.key,
+                        'user': user_serializer.data,
+                        'message': 'Login Success 2!'
+                    }, status = status.HTTP_201_CREATED)
             else:
-                return Response({'error': 'No esta autirizado!'}, status = status.HTTP_401_UNAUTHORIZED)
+                return Response({'error': 'No esta autorizado!'}, status = status.HTTP_401_UNAUTHORIZED)
         else:
             return Response({'error': 'Nombre o password incorrecto'}, status.HTTP_400_BAD_REQUEST)
         return Response({'message':'Todo bien bitches'}, status = status.HTTP_200_OK)
