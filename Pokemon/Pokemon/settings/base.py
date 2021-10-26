2f6f253fd0c53d1f35f4211e5c1a1d92e4379844
Eric Carvajal <carvajal.cortes.e19@gmail.com>
2021-10-26 15:09:38 -0300

    V15 Solucione el problema de las imagenes git log :)

--- Pokemon/Pokemon/settings/base.py
+++ Pokemon/Pokemon/settings/base.py
@@ -123,5 +123,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = 'settings/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "settings/media")
