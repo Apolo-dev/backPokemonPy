d5b5c2fbe9bd714f820d251c8490c616b9365f45
Eric Carvajal <carvajal.cortes.e19@gmail.com>
2021-10-29 20:48:10 -0300

    V18 Agregue los legendarios

--- Pokemon/AppPrincipal/serializers.py
+++ Pokemon/AppPrincipal/serializers.py
@@ -1,6 +1,3 @@
-from typing import List
-from django.db import models
-from django.db.models import fields
 from .models import Ataque, Pokemon
 from rest_framework import serializers
 
--- Pokemon/AppPrincipal/urls.py
+++ Pokemon/AppPrincipal/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 #from Pokemon.AppPrincipal.models import Ataque
-from .views import FantasmaViewset, HieloViewset, LuchaViewset, PrincipalPokemon, AtaquesPokemon, FuegoPokemon, DragonPokemon, DragonViewset, FuegoViewset, PsiquicoViewset, TierraViewset, AguaViewset, ElectricoViewset, VoladorViewset
+from .views import FantasmaViewset, HieloViewset, LuchaViewset, DragonViewset, FuegoViewset, PsiquicoViewset, TierraViewset, AguaViewset, ElectricoViewset, VoladorViewset, LegendarioViewset
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -15,20 +15,13 @@ router.register('psiquico', PsiquicoViewset)
 router.register('lucha', LuchaViewset)
 router.register('fantasma', FantasmaViewset)
 router.register('volador', VoladorViewset)
-
-
+router.register('legendario', LegendarioViewset)
 
 
 
 
 urlpatterns=[
-    path('pokemons/', PrincipalPokemon.as_view(), name='pokemons'),
-    path('pokemons/<int:id>', PrincipalPokemon.as_view(), name='pokemons_process'),
-    path('attacks/', AtaquesPokemon.as_view(), name='attacks'),
-    path('attacks/<int:id>', AtaquesPokemon.as_view(), name='attacks_process'),
-
-    path('pokemons/fuego/', FuegoPokemon.as_view(), name='fuego'),
-    path('pokemons/dragon/', DragonPokemon.as_view(), name='dragon'),
+    
     #path con django rest framework
     path('', include(router.urls)),
 ]
